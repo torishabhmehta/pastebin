070e4312aef591b183c526c8d7548d234a19c759
torishabhmehta <torishabhmehta@gmail.com>
2018-03-29 05:10:24 +0530

    final commit

--- snippets/forms.py
+++ snippets/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-
+from django.forms import ModelForm
+from snippets.models import Comment
 class SnippetForm(forms.Form):
 	title = forms.CharField(label='Title', max_length=30)
 	content = forms.CharField(label='Content', widget=forms.Textarea)
 
+class Commentform(forms.ModelForm):
+	class Meta:
+		model=Comment
+		fields=['content']
+	
--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from snippets.forms import SnippetForm
+from django.http import HttpResponse, Http404
+from snippets.forms import SnippetForm, Commentform
 from snippets.models import Snippet
 # Create your views here.
 
@@ -17,4 +17,18 @@ def create(request):
 	return render(request, 'snippets/create.html', {'form':form})
 
 def view(request, key):
-	return HttpResponse("View function: " + str(key))
+	if Snippet.objects.filter(id=key).exists():
+		snippet=Snippet.objects.get(id=key)
+	else:
+		raise Http404("Snippet does not exist")
+	
+	if request.method == "POST":
+		form=Commentform(request.POST)
+		print(form.is_valid())
+		if form.is_valid():
+			comment=form.save(commit=False)
+			comment.snippet=snippet
+			comment.save()
+	form=Commentform()
+	comments= snippet.comment_set.order_by('-datetime_created')
+	return render(request, 'snippets/view.html', {'key' : key, 'title':snippet.title,'content': snippet.content, 'form':form, 'comments':comments}) 
