2298b2585a69547a86ea7149866cdf2060130ecc
torishabhmehta <torishabhmehta@gmail.com>
2018-03-29 03:34:17 +0530

    snippets made

---
+++ snippets/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class SnippetForm(forms.Form):
+	title = forms.CharField(label='Title', max_length=30)
+	content = forms.CharField(label='Content', widget=forms.Textarea)
+
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from snippets.forms import SnippetForm
+from snippets.models import Snippet
 # Create your views here.
+
+def create(request):
+	if request.method=="GET":
+		form = SnippetForm()
+	elif request.method=="POST":
+		form=SnippetForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			snippet = Snippet(title=data.get('title'), content=data.get('content'))
+			snippet.save()
+			return redirect('view', snippet.id)	
+	return render(request, 'snippets/create.html', {'form':form})
+
+def view(request, key):
+	return HttpResponse("View function: " + str(key))
